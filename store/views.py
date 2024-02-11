5f14adc9b95dad15a6aca4e1d22ce522d154dc99
AbhiSmith <abhishekv4004@gmail.com>
2024-02-11 19:39:48 +0530

    registered process complete

--- store/views.py
+++ store/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models  import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
+
+
 
 
 # Create your views here.
@@ -34,3 +39,24 @@ def  logout_user(request):
     messages.success(request, ("You have been logged out!"))
     return redirect('home')
     
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # log in user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You have successfully registered"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Error registering - please try again"))
+            return redirect('register')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+        
