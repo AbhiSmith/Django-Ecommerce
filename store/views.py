2d76955fbd068374b2ab7372e57c34b5fb4dae69
AbhiSmith <abhishekv4004@gmail.com>
2024-02-12 14:38:25 +0530

    Product Category implement

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from .models  import Product
+from .models  import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
@@ -7,10 +7,25 @@ from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
 from django import forms
 
-
-
-
 # Create your views here.
+def category(request, foo):
+    foo = foo.replace('-', ' ')
+    try:
+        # Look up category
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'category':category, 'products':products})
+    except:
+        messages.success(request, ("That category does not Exist"))
+        return redirect('home')
+
+
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product':product})
+
+
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products':products})
